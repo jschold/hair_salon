39d7a2679bec10da6f7bf8b33e94d0dec370a23a
jschold <jscholdny@msn.com>
2015-08-21 15:47:53 -0700

    add working index twig and faulty stylists twig.

--- app/app.php
+++ app/app.php
@@ -25,12 +25,25 @@
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+    $app->get("/stylists/{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+
     $app->post("/stylists", function() use ($app) {
-        $stylist = new Stylist($_POST['name']);
+        $stylist = new Stylist($_POST['client_name']);
         $stylist->save();
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+    $app->post("/stylists", function() use ($app) {
+        $client_name = $_POST['client_name'];
+        $stylist_id = $_POST['stylist_id'];
+        $client = new Client($id = null, $client_name, $stylist_id);
+        $client->save();
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
 
     return $app;
 
