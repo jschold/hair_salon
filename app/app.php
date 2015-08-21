cec2f63dbbc3df1455ccac2fac504bc4be965b4e
jschold <jscholdny@msn.com>
2015-08-21 16:28:42 -0700

    add patch function to app.php.

--- app/app.php
+++ app/app.php
@@ -34,6 +34,24 @@
         return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
     });
 
+    $app->get("/stylists/{id}/edit", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('edit.html.twig', array('stylist' => $stylist));
+    });
+
+    $app->patch("/stylists/{id}", function($id) use ($app) {
+        $name = $_POST['name'];
+        $stylist = Stylist::find($id);
+        $stylist->update($name);
+        return $app['twig']->render('stylists.html.twig', array('stylist' =>$stylist, 'clients' => $stylist->getClients()));
+    });
+
+    $app->delete("/stylists/{id}", function($id) use ($app) {
+        $cuisine = Stylist::find($id);
+        $cuisine->delete();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
     $app->post("/stylists", function() use ($app) {
         $stylist = new Stylist($_POST['name']);
         $stylist->save();
