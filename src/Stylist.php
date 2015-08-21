1faa75c898006170f8f6591dc5ae98a9ed978541
jschold <jscholdny@msn.com>
2015-08-21 12:42:23 -0700

    add tests for getName, save, getAll, and deleteAll, and methods to make them pass in Stylist.php.

--- src/Stylist.php
+++ src/Stylist.php
@@ -1,6 +1,59 @@
 <?php
   class Stylist
   {
-    
+      private $name;
+      private $id;
+
+      function __construct($name, $id = null)
+      {
+          $this->name = $name;
+          $this->id = $id;
+      }
+
+      function setName($new_name)
+      {
+          $this->name = (string) $new_name;
+      }
+
+      function getName()
+      {
+          return $this->name;
+      }
+
+      function setId($new_id)
+      {
+          $this->id = $new_id;
+      }
+
+      function getId()
+      {
+          return $this->id;
+      }
+
+      function save()
+      {
+          $GLOBALS['DB']->exec("INSERT INTO stylists (name) VALUES ('{$this->getName()}');");
+          $result_id = $GLOBALS['DB']->lastInsertId();
+          $this->setId($result_id);
+      }
+
+      static function getAll()
+      {
+          $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+          $stylists = array();
+          foreach($returned_stylists as $stylist) {
+              $name = $stylist['name'];
+              $id = $stylist['id'];
+              $new_stylist = new Stylist($name, $id);
+              array_push($stylists, $new_stylist);
+          }
+          return $stylists;
+      }
+
+      static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
+        }
+
   }
 ?>
